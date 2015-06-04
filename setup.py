1e626b3eb30b3cf7b450c54386d46997c7055725
Juti Noppornpitak <jnopporn@shiroyuki.com>
2015-06-04 01:44:24 -0400

    Updated for release

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
-from distutils.core import setup
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
 
 setup(
     name         = 'Kotoba',
     version      = '3.1.0',
-    description  = 'XML Reading Library',
+    description  = 'XML/JSON Reading Library with Level-3 CSS Selector',
     author       = 'Juti Noppornpitak',
     author_email = 'juti_n@yahoo.co.jp',
     url          = 'http://shiroyuki.com/work/projects-kotoba',
